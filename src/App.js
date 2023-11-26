30fe16c478853c3ce7dc6fee9c9d13b0158b6755
profiler00 <kftjungle@gmail.com>
2023-11-26 19:23:34 +0900

    feat(App): 기능2 이동 시도 횟수 입력 메서드 구현 완료

--- src/App.js
+++ src/App.js
@@ -10,14 +10,12 @@ class App {
     );
 
     // TODO: Mission 2: 이동 시도 횟수 입력 메서드입니다.
-
-    // TODO: Mission 3: 자동차 이동 로직 메서드입니다.
-
-    // TODO: Mission 4: 각 차수별 실행 결과 출력 메서드입니다.
+    const totalAttempts = parseInt(
+      await MissionUtils.Console.readLineAsync('시도할 횟수를 입력하세요:'),
+      10,
+    );
 
     // TODO: Mission 5: 오류 처리 메서드입니다.
-
-    // TODO: Mission 6: 우승자 결정 메서드입니다.
   }
 }
 
