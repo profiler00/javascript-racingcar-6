aea7fa2d55305a5b4f0b34876defa86077a4520f
profiler00 <kftjungle@gmail.com>
2023-11-26 21:44:32 +0900

    test(InputValueTest): 입력값 유효성 검사 테스트 코드 작성 및 추가

---
+++ __tests__/InputValueTest.js
@@ -0,0 +1,25 @@
+import Validation from '../src/validations/Validation';
+
+describe('입력 값 테스트', () => {
+  test('값이 입력되지 않았을 경우, 예외 처리를 한다.', () => {
+    const inputValue = ['', null, undefined];
+
+    inputValue.forEach(input => {
+      expect(() => Validation.checkNull(input)).toThrow(
+        '[ERROR] 값을 입력해주세요.',
+      );
+    });
+  });
+
+  test('각 자동차 이름이 6자 이상인 경우, 예외 처리를 한다.', () => {
+    expect(() => Validation.checkNameLength(['mynameiskelvinlee'])).toThrow(
+      '[ERROR] 각 입력값은 5자 이하만 가능합니다.',
+    );
+  });
+
+  test('숫자가 아닌 값이 입력된 경우, 예외 처리를 한다.', () => {
+    expect(() => Validation.checkCount('string')).toThrow(
+      '[ERROR] 시도 횟수는 양의 정수여야 합니다.',
+    );
+  });
+});
